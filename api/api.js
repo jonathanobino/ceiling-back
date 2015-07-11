27f4d61680657b8fee0eb505145307ca10ff083d
Jonathan Obino <Jonathan@MacBook-Pro-di-Jonathan.local>
2015-07-11 16:15:22 +0200

    aggiunte api e schemi

---
+++ api/api.js
@@ -0,0 +1,14 @@
+var api={
+	restaurants: function(req,res,next){
+
+	},
+	restaurants_id:function(req,res,next){
+
+	},
+	restaurants_id_cat: function(req,res,next){
+
+	},
+	pay: function(req,res,next){
+		
+	}
+}
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var config = require('./config/config');
+var restaurant = require('./models/restaurant');
 var app = express();
+
+var dbUrl = config.base + config.user+":"+config.psw+config.uri;
+
+mongoose.connect(dbUrl,function(err){
+	if(err) console.log(err);
+	console.log('connected');
+});
 
 
 app.get('/', function(req,res){
-	res.send("Hello world!");
+	res.send("It's working!");
 })
 
 
