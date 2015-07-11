184c41875dcdbb2e1040569d7c5c486d77dc4384
Jonathan Obino <Jonathan@MacBook-Pro-di-Jonathan.local>
2015-07-11 17:37:18 +0200

    api

--- api/api.js
+++ api/api.js
@@ -1,14 +1,39 @@
+var mongoose = require('mongoose');
+var Restaurant = mongoose.model('Restaurant', require('../models/restaurant'));
+
 var api={
-	restaurants: function(req,res,next){
+	restaurants: function(req,res){
+		Restaurant.find({}).exec(function(err,result){
+		res.send(result);
+	});
 
 	},
-	restaurants_id:function(req,res,next){
+	restaurants_id:function(req,res){
+		var id= req.params.id;
+
+		Restaurant.find({
+			_id:id
+		})
+		.exec(function(err,result){
+			res.send(result);
+		})
+	},
+	restaurants_id_cat: function(req,res){
+		var id = req.params.id;
+		var cat = req.params.cat;
+
+		Restaurant.find({
+			_id:id,
+		})
+		.exec(function(err,result){
+			if(err) res.send(err);
+			res.send(result[0].menu[cat]);
+		})
 
 	},
-	restaurants_id_cat: function(req,res,next){
+	pay: function(req,res){
 
-	},
-	pay: function(req,res,next){
-		
 	}
 }
+
+module.exports= api;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var mongoose = require("mongoose");
 var config = require('./config/config');
 var Restaurant = mongoose.model('Restaurant', require('./models/restaurant'));
+var api = require('./api/api');
 var app = express();
 
 var dbUrl = config.base + config.user+":"+config.psw+config.uri;
@@ -13,15 +14,16 @@ mongoose.connect(dbUrl,function(err){
 
 
 app.get('/', function(req,res){
-	// Restaurant.create({
-	// 	name:"H-farm",
-	// 	address:"Via Sile",
-	// 	city:"Roncade"
-	// }).save();
 	var new_res = new Restaurant({
 		name:"H-farm",
 		address:"Via Sile",
-		city:"Roncade"
+		city:"Roncade",
+		menu:{
+			sides:[{
+				name: "Insalata",
+				price:"$10.00"
+			}]
+		}
 	});
 
 	new_res.save(function(err,new_res){
@@ -32,7 +34,13 @@ app.get('/', function(req,res){
 			res.send(new_res)
 		}
 	})
-})
+});
+
+app.get('/restaurants', api.restaurants);
+
+app.get('/restaurants/:id',api.restaurants_id);
+
+app.get('/restaurants/:id/:cat', api.restaurants_id_cat);
 
 
 
