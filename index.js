c8b73795503182e9842438337057d639d068af26
Jonathan Obino <Jonathan@MacBook-Pro-di-Jonathan.local>
2015-07-11 16:27:32 +0200

    working db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var config = require('./config/config');
-var restaurant = require('./models/restaurant');
+var Restaurant = mongoose.model('Restaurant', require('./models/restaurant'));
 var app = express();
 
 var dbUrl = config.base + config.user+":"+config.psw+config.uri;
@@ -13,7 +13,25 @@ mongoose.connect(dbUrl,function(err){
 
 
 app.get('/', function(req,res){
-	res.send("It's working!");
+	// Restaurant.create({
+	// 	name:"H-farm",
+	// 	address:"Via Sile",
+	// 	city:"Roncade"
+	// }).save();
+	var new_res = new Restaurant({
+		name:"H-farm",
+		address:"Via Sile",
+		city:"Roncade"
+	});
+
+	new_res.save(function(err,new_res){
+		if(err){
+			res.send(err);
+		}
+		else{
+			res.send(new_res)
+		}
+	})
 })
 
 
