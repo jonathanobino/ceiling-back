87c4cafb14f601f6ae2a3a57c9e37d968bd0fb16
Jonathan Obino <Jonathan@MacBook-Pro-di-Jonathan.local>
2015-07-11 12:58:17 +0200

    get

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require("express");
 var app = express();
 
 
-// app.get('/', function(req,res){
-// 	res.send("Hello world!");
-// })
+app.get('/', function(req,res){
+	res.send("Hello world!");
+})
 
 
 
